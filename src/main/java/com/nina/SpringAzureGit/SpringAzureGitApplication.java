0c9db59d5fef01b318bce11c34e5d882c7b01b84
Naveen <NiNa@Naveens-MacBook-Pro.local>
2020-03-18 14:20:16 +0400

    adding all files

---
+++ src/main/java/com/nina/SpringAzureGit/SpringAzureGitApplication.java
@@ -0,0 +1,13 @@
+package com.nina.SpringAzureGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAzureGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nina/SpringAzureGit/SpringAzureGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nina.SpringAzureGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
